a7407e1a1175845c0e569cc3528cdeb47dbea420
Adriana <103216065+facundocaro1826@users.noreply.github.com>
2023-12-15 00:04:16 -0300

    agregando archivos de mercadoLiebre

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express()
+const path = require('path')
+const port = 3030;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get('/home', (req, res)=>{
+    res.redirect('/')
+})
+
+
+
+app.listen(port, ()=> console.log(`htt://localhost: ${port}`))
